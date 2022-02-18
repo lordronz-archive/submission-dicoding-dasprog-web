2b6fad1aaa729721c52d1b2657d8e8f03bf750fd
LordRonz <aarontanhar2000@gmail.com>
2022-02-18 23:40:28 +0700

    feat: use js to render characters, store like count in localStorage :rocket:

--- website/script.js
+++ website/script.js
@@ -1,3 +1,5 @@
+// Note: I'm avoiding arrow function for compatibility purpose
+
 function oldCopyToClipboard(text) {
   temp = document.createElement('textarea');
   temp.value = text;
@@ -26,3 +28,86 @@ function copyEmail() {
   }
   copyToClipboard(email);
 }
+
+// Card structure (for development)
+{
+  /* <div class="character-card">
+  <figure class="character-picture">
+    <img src="assets/characters/kaworu.jpg" alt="" />
+    <figcaption>
+      <a
+        href="https://anilist.co/character/1261/"
+        class="animated-underline underline-link"
+        target="_blank"
+        rel="noopener noreferrer"
+        >Kaworu Nagisa</a
+      >
+    </figcaption>
+  </figure>
+  <button class="button">Like</button>
+</div> */
+}
+
+(function () {
+  const charsObj = JSON.parse(localStorage.getItem('characters')) || {
+    kaworu: {
+      name: 'Kaworu Nagisa',
+      img: 'assets/characters/kaworu.jpg',
+      link: 'https://anilist.co/character/1261/',
+      like: 0,
+    },
+    asuka: {
+      name: 'Asuka Langley Soryu',
+      img: 'assets/characters/asuka.jpg',
+      link: 'https://anilist.co/character/94/',
+      like: 0,
+    },
+    shinji: {
+      name: 'Shinji Ikari',
+      img: 'assets/characters/shinji.jpg',
+      link: 'https://anilist.co/character/89/',
+      like: 0,
+    },
+  };
+
+  localStorage.setItem('characters', JSON.stringify(charsObj));
+
+  const charContainer = document.querySelector('.characters-container');
+
+  Object.keys(charsObj).forEach((char) => {
+    const characterCard = document.createElement('div');
+    characterCard.classList.add('character-card');
+    const characterFig = document.createElement('figure');
+    characterFig.classList.add('character-picture');
+    const characterImg = document.createElement('img');
+    characterImg.src = charsObj[char].img;
+    characterImg.alt = '';
+    const characterFigCaption = document.createElement('figcaption');
+    const characterLink = document.createElement('a');
+    characterLink.classList.add('animated-underline', 'underline-link');
+    characterLink.href = charsObj[char].link;
+    characterLink.target = '_blank';
+    characterLink.rel = 'noopener noreferrer';
+    characterLink.innerHTML = charsObj[char].name;
+    const characterButton = document.createElement('button');
+    characterButton.classList.add('button');
+    characterButton.innerHTML =
+      'Like (' + Math.max(0, Math.min(999, charsObj[char].like)) + ')';
+    characterButton.id = char;
+
+    characterButton.addEventListener('click', function () {
+      const charBtn = document.querySelector('#' + char);
+      const chars = JSON.parse(localStorage.getItem('characters')) || charsObj;
+      const like = ++chars[char].like;
+      charBtn.innerHTML = 'Like (' + Math.max(0, Math.min(999, like)) + ')';
+      localStorage.setItem('characters', JSON.stringify(chars));
+    });
+
+    characterFigCaption.appendChild(characterLink);
+    characterFig.appendChild(characterImg);
+    characterFig.appendChild(characterFigCaption);
+    characterCard.appendChild(characterFig);
+    characterCard.appendChild(characterButton);
+    charContainer.appendChild(characterCard);
+  });
+})();
